4771d7ba115732f6b101afabb18f009c7401f7f9
ch3ng666 <gabrielaloi90@gmail.com>
2024-06-13 18:33:35 -0300

    1306-2

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 '''App'''
 import flet as ft
-# import time
+from usd_mayorista import usd_mayorista_value
 
 
 def main(page: ft.Page):
-    def button_clicked(e):
-        output_text.value = f"Dropdown value is:  {color_dropdown.value}"
+    'main'
+    page.bgcolor = '#000000'
+    page.window_height = 768
+    page.window_width = 432
+
+    def reload_button(e):  # pylint: disable=unused-argument
+        usd_mayorista.update()
         page.update()
 
-    output_text = ft.Text()
-    submit_btn = ft.ElevatedButton(text="Submit", on_click=button_clicked)
-    color_dropdown = ft.Dropdown(
-        width=100,
-        options=[
-            ft.dropdown.Option("Red"),
-            ft.dropdown.Option("Green"),
-            ft.dropdown.Option("Blue"),
-        ],
+    page.appbar = ft.AppBar(
+        title=ft.Text(value='ECONOMY DATA', text_align='center',
+                      italic=True, weight='bold', size=20, color='indigo999'),
+        center_title=True,
+        bgcolor=ft.colors.BLACK,
+        actions=[ft.Container(
+            content=ft.IconButton(ft.icons.REFRESH_ROUNDED,
+                                  on_click=reload_button)
+        )
+
+        ]
     )
-    page.add(color_dropdown, submit_btn, output_text)
+
+    usd_mayorista = ft.Row(
+        [
+            ft.Text(value='USD Mayorista:', weight='bold', text_align='right'),
+            ft.Text(value=usd_mayorista_value)
+        ], alignment=ft.MainAxisAlignment.CENTER, spacing=100
+    )
+
+    page.add(usd_mayorista)
 
 
 ft.app(main)
